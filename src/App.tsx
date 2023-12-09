aa71e9a445b0b773289c556376e06f88bab4ed81
justbenuk <justbenuk@gmail.com>
2023-12-09 16:22:52 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Input from "./components/Input";
 import Button from "./components/Button";
+import { useRef } from "react";
 
 function App() {
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  const ageRef = useRef<HTMLInputElement>(null)
+
   return (
     <main>
       <p>Example Inputfields</p>
-      <Input label="Your Name" id="name" type='text'/>
-      <Input label="Your Age" id="age" type='number'/>
+      <Input label="Your Name" id="name" type='text' ref={inputRef}/>
+      <Input label="Your Age" id="age" type='number' ref={ageRef}/>
       <hr />
       <p>Example Button Components</p>
       <Button el="button" className="button">Click Me</Button>
